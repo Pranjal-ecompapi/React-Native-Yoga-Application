7a47e411027f2dbed831b54c4006d38318ecbc37
Pranjal-ecompapi <pranjal@ecompapi.com>
2024-07-23 12:23:32 +0530

    Create the splash screen home page and welcome SCREEN OF the app with bottom bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,44 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsLayout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+        tabBarActiveTintColor: Colors.primary, // Active tab color
+        
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="nature-meditate"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          tabBarLabel: 'Meditate',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons 
+                name="flower-tulip"     
+                size={size} 
+                color={color} />
           ),
         }}
       />
-      <Tabs.Screen
-        name="explore"
+
+<Tabs.Screen
+        name="affirmation"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          tabBarLabel: 'affirmation',
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="open-book" size={24} color="black" />
           ),
         }}
       />
+      {/* Add more Tabs.Screen as needed */}
     </Tabs>
-  );
+  )
 }
+
+export default TabsLayout
---
+++ app/(tabs)/affirmation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Affirmation = () => {
+  return (
+    <View>
+      <Text>Affirmation</Text>
+    </View>
+  )
+}
+
+export default Affirmation
